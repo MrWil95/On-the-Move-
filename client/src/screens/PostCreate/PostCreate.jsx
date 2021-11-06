955e753c6c578419d31b91b0452333915f8ed546
MrWil95 <alexwilbur5991@gmail.com>
2021-11-05 21:38:25 -0500

    working on create

--- client/src/screens/PostCreate/PostCreate.jsx
+++ client/src/screens/PostCreate/PostCreate.jsx
@@ -1,31 +1,71 @@
 import { useState } from 'react'
+import { getCategories } from '../../services/posts'
 
 export default function PostCreate() {
-  const []
+  const [posts, setPosts] = useState([])
+  const [categories, setCategories] = useState([])
+  const [selectedCategory, setSelectedCategory] = useState('')
   const [formData, setFormData] = useState({
-    name: '',
-  });
-  const { name } = formData;
+    content: '',
+    img_url: '',
+    link_url: '',
+  })
+  const { content, img_url, link_url } = formData
 
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const postList = await getCategories();
+      setCategories(postList);
+    };
+    fetchCategories();
+  }, []);
 
   const handleChange = (e) => {
-    const { value } = e.target;
+    const { value } = e.target
     setFormData({
-      name: value,
-    });
-  };
+      content: value,
+      img_url: value,
+      link_url: value,
+    })
+  }
+
+  const handleCategoryChange = (e) => {
+    const { value } = e.target
+    setSelectedCategory(value)
+  }
+
+  const handlePostCreate = async (formData) => {
+    const newFood = await createPost(formData)
+    setPosts((prevState) => [...prevState, newFood])
+    history.push('/foods')
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const foodItem = await addFlavorToFood(selectedFlavor, id)
+    setFoodItem(foodItem)
+  }
 
   return (
     <form
       onSubmit={(e) => {
-        e.preventDefault();
-        handleFoodCreate(formData);
+        e.preventDefault()
+        handlePostCreate(formData)
       }}
     >
-      <h1>Create Food</h1>
       <label>
-        Name:
-        <input type='text' value={name} onChange={handleChange} />
+        Message:
+        <textarea type='text' value={content} onChange={handleChange} />
+      </label>
+      <br />
+      <label>
+        Image:
+        <input type='text' value={img_url} onChange={handleChange} />
+      </label>
+      <br />
+      <label>
+        Link:
+        <input type='text' value={link_url} onChange={handleChange} />
       </label>
       <br />
       <button>Submit</button>
