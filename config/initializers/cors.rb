b1abfd5282c01f9abe5fa9ea914a9e3dbd13fb1f
MrWil95 <alexwilbur5991@gmail.com>
2021-11-12 15:36:33 -0600

    fixed

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://on-the-move.netlify.app'
+    origins ['https://on-the-move.netlify.app', 'localhost:3001']
 
     resource '*',
       headers: :any,
