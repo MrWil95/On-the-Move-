bb8ffa8b904c8d35cf66e97e54794ab3e794f702
MrWil95 <alexwilbur5991@gmail.com>
2021-11-12 14:36:41 -0600

    fix3

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = process.env.NODE_ENV === 'production' ? ' cd general_assembly/hummingbirds/unit_4/project/On-the-Move-/' : 'http://localhost:3000' 
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://on-the-move.heroku.com' : 'http://localhost:3000' 
 
 export const api = axios.create({
   baseURL: baseUrl
