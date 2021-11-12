03e05c87f1db97d60af1a0681b19193132c50989
MrWil95 <alexwilbur5991@gmail.com>
2021-11-12 14:48:54 -0600

    fix 7

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -3,9 +3,6 @@ import axios from 'axios'
 const baseUrl = process.env.NODE_ENV === 'production' ? 'https://on-the-move.heroku.com' : 'http://localhost:3000' 
 
 export const api = axios.create({
-  baseURL: baseUrl,
-  headers: {
-	  'Access-Control-Allow-Origin': '*',
-	}
+  baseURL: baseUrl
 })
 
