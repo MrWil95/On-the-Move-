644ed7cb8c4c5e9bfa0f6cae4c770b09f9dcafeb
MrWil95 <alexwilbur5991@gmail.com>
2021-11-17 14:08:17 -0600

    fixing styling

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,14 +1,15 @@
+import './Layout.css'
 import NavBar from '../Nav/NavBar'
 
 export default function Layout(props) {
   const { children, currentUser, handleLogout } = props
   return (
-    <div className='LayoutContianer' style={{ height: "5em"}}>
+    <div className='LayoutContianer'>
       <NavBar 
         currentUser={currentUser}
         handleLogout={handleLogout}
       />
-      <div className='layoutchildren' style={{position: "absolute", top: "8.5em", zIndex: "-5"}}>
+      <div className='layoutchildren'>
         {children}
       </div>
     </div>
